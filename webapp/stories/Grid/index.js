da15784dad53d7afaeeeeb8fa09c9eb967611f3b
Lucas Duarte <lc.sales.duarte@gmail.com>
2017-09-15 16:04:07 -0300

    grid: add combination examples

--- webapp/stories/Grid/index.js
+++ webapp/stories/Grid/index.js
@@ -15,6 +15,7 @@ import '../../src/styles/index.css'
 
 const gridMaxQty = 12
 const equalDistributionExamples = [1, 2, 3, 4, 6, 12]
+const combinationExamples = range(3, 9)
 
 storiesOf('Grid/Desktop', module)
   .add('Equal distribuition', () => (
@@ -35,6 +36,39 @@ storiesOf('Grid/Desktop', module)
               </Card>
             </Col>
           ))}
+        </Row>
+      ))}
+    </Grid>
+  ))
+
+  .add('Combinations', () => (
+    <Grid>
+      {combinationExamples.map(size => (
+        <Row>
+          <Col desktop={size}>
+            <Card>
+              <CardTitle>
+                Hello World {size}
+              </CardTitle>
+              <CardText>
+                CardTitle is defined but never used 
+                no-unused-vars, CardTitle is defined
+                but never used  no-unused-vars
+              </CardText>
+            </Card>
+          </Col>
+          <Col desktop={gridMaxQty - size}>
+            <Card>
+              <CardTitle>
+                Hello World {gridMaxQty - size}
+              </CardTitle>
+              <CardText>
+                CardTitle is defined but never used 
+                no-unused-vars, CardTitle is defined
+                but never used  no-unused-vars
+              </CardText>
+            </Card>
+          </Col>
         </Row>
       ))}
     </Grid>
